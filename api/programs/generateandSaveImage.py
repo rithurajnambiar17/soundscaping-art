2779b4fa76670bb9cf2c0e740c08333aa639ad6d
rithurajnambiar <rithurajnambiar17@gmail.com>
2023-04-20 11:32:25 +0530

    and the api is working!

---
+++ api/programs/generateandSaveImage.py
@@ -0,0 +1,31 @@
+import io
+import warnings
+from PIL import Image
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+def generate_and_save_image(api, prompt, path, cfg_scale=8.0, noImage=1):
+    width, height = 512, 512
+    sampler = generation.SAMPLER_K_DPMPP_2M
+
+    # Generate answer
+    answer = api.generate(
+        prompt=prompt,
+        sampler=sampler,
+        width=width,
+        height=height,
+        cfg_scale=cfg_scale,
+        samples = noImage
+    )
+
+    for resp in answer:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Saftety Filters have been triggered. Modify the prompt and try again."
+                )
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save(str(prompt) + ".png")
+                return True
+            else:
+                return False
