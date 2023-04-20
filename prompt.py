12163843cb2978dde507aa974c0f7f973e7ac800
CR1502 <craig.lionel2020@vitbhopal.ac.in>
2023-04-20 10:54:20 +0530

    one step closer to winning!

---
+++ prompt.py
@@ -0,0 +1,62 @@
+from SoundClassification import extract_feature_and_print_prediction
+import random
+
+air_conditioner = ["Amidst a scorching heatwave, an air conditioner stands tall like a beacon of hope. Its cool breeze offers respite to anyone seeking refuge from the sweltering heat. In this dreamlike painting, capture the air conditioner as an oasis of coolness, surrounded by shimmering heatwaves that distort reality. Let your imagination run wild and infuse the scene with a sense of tranquility, as if the air conditioner is a magical portal to a world of eternal coolness.",
+                   "Amidst a scorching heatwave, a lone air conditioner stands tall like an oasis of coolness. Its gentle hum and icy breath offer a respite from the sweltering heat outside. As you gaze at the machine, you cant help but wonder about the secrets of its cooling powers and the relief it brings to those seeking refuge from the unforgiving sun.",
+                   "Create a vivid and realistic painting of an air conditioner. Show the intricate details of its components, the sleekness of its design, and the coolness it brings to a room on a hot summer day. Use your artistic abilities to make the viewer feel the refreshing breeze it provides",
+                   "I would like you to paint me a picture of an air conditioner. Please use your creativity to depict an air conditioner in a way that is visually appealing and interesting. Consider the form, texture, and color of the air conditioner, as well as its context and surroundings. I look forward to seeing your interpretation of this everyday object."]
+
+children_playing= ["I would love for you to paint me a picture of children playing. Imagine a scene where children are outside, perhaps in a park or a playground, engaged in various activities. You could show them running, jumping, climbing, or playing games. Consider the expressions on their faces, the clothes they are wearing, and the environment around them. Please create a vibrant and lively scene that captures the joy and energy of childhood play. I am excited to see your artistic interpretation!",
+            "Can you please paint me a picture of children playing in the snow? Picture a snow-covered landscape with children running, sliding, and building snowmen. Imagine the snowflakes falling gently from the sky, and the laughter and excitement of the children as they play. You could incorporate colorful winter clothing, sleds, and snowballs into the scene, and perhaps show some children sipping hot cocoa to warm up. Please create a joyful and lively scene that captures the magic of winter play. I can't wait to see your artistic interpretation!",
+            "I would like you to paint me a picture of children playing at the beach. Picture a scene where the children are building sandcastles, collecting seashells, and splashing in the waves. You could show some children flying kites or playing beach volleyball in the background. Consider the bright colors of beach towels, swimsuits, and umbrellas, and the vastness of the ocean and sky. Please create a lively and playful scene that captures the carefree spirit of childhood at the beach. I am excited to see your artistic interpretation",
+            "I would love for you to paint me a picture of children playing in a field of flowers. Imagine a meadow filled with colorful flowers, and children running through the field, picking flowers, and playing games. Consider the bright and vibrant colors of the flowers, the softness of the grass, and the joy and laughter on the faces of the children. You could include elements such as a picnic blanket, a frisbee, or a kite to add to the playful atmosphere. Please create a scene that captures the happiness and wonder of childhood, surrounded by the beauty of nature."]
+
+car_horn =["I would like you to paint me a picture of car horns. Picture a busy street with cars honking their horns. Consider the different tones and rhythms of the horns, and the way they create a symphony of sound. You could show the cars themselves, or perhaps just the horns floating in the air. Consider the colors and textures of the street and the cars, and the way the honking horns add to the energy and chaos of the scene. Please create a dynamic and visually interesting scene that captures the essence of car horns in a city.",
+           "Paint a picture of a chaotic city intersection with cars honking their horns in frustration. The scene should convey a sense of rush hour traffic, with multiple cars and trucks jostling for position and impatiently blaring their horns. The colors should be bright and bold, with the honking horns adding a layer of sound and energy to the painting.",
+           "Paint a picture of a classic car show, with vintage cars from different eras lined up and their owners proudly showing them off. In the foreground of the painting, there should be a close-up of a classic car's horn, polished and gleaming, with intricate details that capture the craftsmanship of a bygone era. The background should be filled with other cars and people admiring them, but the focus should be on the beauty of the classic car horn. The colors should be rich and vibrant, with the horn shining in the light and the surrounding elements adding to the nostalgic atmosphere.",
+           "Paint a picture of a vintage car with a unique and distinctive horn? The car should be set against a beautiful and scenic backdrop, perhaps with rolling hills or a sunset in the distance. The horn should be the centerpiece of the painting, with intricate details and a sense of elegance and charm. The colors should be warm and inviting, and the overall tone of the painting should convey a sense of nostalgia and a bygone era"]
+
+dog_bark = ["Paint a lively scene of dogs barking in a park? The painting should capture the joy and energy of the dogs as they run and play, with their barks filling the air. The colors should be bright and vibrant, with the green grass and blue sky providing a beautiful backdrop for the playful pups. You could also include their owners, perhaps sitting on a bench and enjoying the lively atmosphere. Overall, the painting should convey a sense of happiness and the simple pleasures of spending time with our furry friends.",
+            "Paint a picture of a pack of dogs barking excitedly in a natural setting, such as a forest or park. The dogs should be a mix of different breeds and sizes, with their tongues lolling out and ears perked up in excitement. The colors of the painting should be earthy and natural, with the scenery complementing the dogs' energy and liveliness. The dogs' barks should be visible in the painting, perhaps in the form of sound waves or speech bubbles, adding a playful and whimsical touch to the overall composition.",
+            "Paint a picture of a pack of playful dogs barking joyously in a grassy field. The scene should be full of energy and movement, with the dogs leaping and bounding through the field as they bark and play. The colors should be vibrant and lively, with the green grass contrasting with the bright and varied fur of the dogs. The painting should capture the joy and excitement of these lovable animals, and convey a sense of their unique personalities and characteristics through their playful barking",
+            "Paint a picture of a group of dogs barking excitedly in a park or open field? The dogs should be a mix of breeds and sizes, all with their tongues lolling out and their tails wagging. The background should be lush and green, with perhaps a few trees or flowers in the distance. The dogs should be the main focus of the painting, with their barks conveyed through dynamic brushstrokes and vivid colors. The overall effect should be one of joy and playfulness, capturing the exuberance and energy of our furry friends."]
+
+drilling = ["",
+            "",
+            "",
+            ""]
+
+engine_idling = ["",
+                 "",
+                 "",
+                 ""]
+
+gun_shot = ["",
+            "",
+            "",
+            ""]
+
+jackhammer = ["",
+              "",
+              "",
+              ""]
+
+siren = ["",
+         "",
+         "",
+         ""] 
+
+street_music = ["",
+                "",
+                "",
+                ""]
+
+print(random.choice(air_conditioner))
+print(random.choice(children_playing))
+print(random.choice(dog_bark))
+print(random.choice(drilling))
+print(random.choice(engine_idling))
+print(random.choice(gun_shot))
+print(random.choice(jackhammer))
+print(random.choice(siren))
+print(random.choice(street_music))
